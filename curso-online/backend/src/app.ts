5fdfe0fec9eb6cde32a63755ee2cc35899320f71
Pedro Igor Veras <pedrohygorveras@gmail.com>
2024-02-22 07:43:22 -0300

    implementing the routes in the initial configuration file

--- curso-online/backend/src/app.ts
+++ curso-online/backend/src/app.ts
@@ -1,14 +1,52 @@
 "use strict";
 
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import http from "http";
+
 import env from "../environments";
 
-const app = express();
+import ApiRoutes from "./routes/ApiRoutes";
+import CategoryRoutes from "./routes/CategoryRoutes";
 
+class App {
+  public app: express.Application;
+  public server: http.Server;
 
-const PORT: number = parseInt(env.APP_PORT) || 8686;
-const HOST: string = env.APP_HOST || "0.0.0.0";
+  constructor() {
+    this.app = express();
+    this.server = http.createServer(this.app);
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+    this.middlewares();
+    this.virtualization();
+    this.routes();
+    this.run();
+  }
+
+  private middlewares(): void {
+    this.app.use(cors());
+
+    this.app.use(express.json({ limit: "50mb" }));
+    this.app.use(express.urlencoded({ limit: "50mb", extended: true }));
+  }
+
+  private virtualization(): void {
+    this.app.use(express.static(__dirname + "/public/"));
+  }
+
+  private routes(): void {
+    this.app.use("/", ApiRoutes);
+    this.app.use("/category", CategoryRoutes);
+  }
+
+  private run(): void {
+    const APP_PORT: number = parseInt(env.APP_PORT) || 8686;
+    const APP_HOST: string = env.APP_HOST || "0.0.0.0";
+
+    this.app.listen(APP_PORT, APP_HOST, () => {
+      console.log(`Server is running.`);
+    });
+  }
+}
+
+export default new App().server;
