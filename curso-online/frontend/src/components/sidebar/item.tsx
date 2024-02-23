44d5ebc9c8f31a4af56bb1ffcac98ec6c8b4987a
Pedro Igor Veras <pedrohygorveras@gmail.com>
2024-02-23 14:52:04 -0300

    correction of link in side menu

--- curso-online/frontend/src/components/sidebar/item.tsx
+++ curso-online/frontend/src/components/sidebar/item.tsx
@@ -18,16 +18,17 @@ const ItemSidebar: React.FC<ItemSidebarProps> = ({ item, index }) => {
   const pathname = usePathname();
 
   return (
-    <li
-      className={`group relative flex flex-col items-center justify-center h-24 px-4 duration-300 ease-in-out border-base-200 border-b
+    <li>
+      <Link
+        href={`/dashboard/${item.link}`}
+        className={`group relative flex flex-col items-center justify-center h-24 px-4 duration-300 ease-in-out border-base-200 border-b
        ${index === 0 && "border-t"}
        ${
          pathname.includes(item.link)
            ? "bg-primary border-bg-primary"
            : "hover:bg-base-200"
        }`}
-    >
-      <Link href={`/dashboard/${item.link}`}>
+      >
         <div>
           <div className="flex flex-col items-center text-2xl">{item.icon}</div>
           <div className="text-xs mt-2">{item.label}</div>
