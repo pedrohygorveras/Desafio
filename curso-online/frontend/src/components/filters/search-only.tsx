1d28a66b8e20a1eec6864063614b4b4b3567fe7c
Pedro Igor Veras <pedrohygorveras@gmail.com>
2024-02-24 08:43:39 -0300

    separating filter component

---
+++ curso-online/frontend/src/components/filters/search-only.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { ChangeEvent, useState } from "react";
+
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+
+import { Input } from "@/components/form/controls";
+import { refreshCache } from "@/components/actions/server";
+
+const Filter: React.FC<{ key: string }> = ({ key }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const [search, setSearch] = useState("");
+
+  function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = String(event.target.value);
+
+    setSearch(value);
+
+    const current = new URLSearchParams(Array.from(searchParams.entries()));
+    current.set("search", value);
+
+    const currentQuery = current.toString();
+    const query = currentQuery ? `?${currentQuery}` : ``;
+
+    refreshCache({
+      key: key,
+    });
+
+    router.push(`${pathname}${query}`);
+  }
+
+  return (
+    <div className="mt-8">
+      <Input
+        label="Buscar"
+        value={search}
+        placeholder="Pesquise por títulos..."
+        length={false}
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
+};
+
+export { Filter };
