07af7b2dd7634c0b82858d5ca536f5baefff7a81
Pedro Igor Veras <pedrohygorveras@gmail.com>
2024-02-24 07:30:58 -0300

    correction in dark mode loading configuration

--- curso-online/frontend/src/components/navbar/dashboard.tsx
+++ curso-online/frontend/src/components/navbar/dashboard.tsx
@@ -1,10 +1,8 @@
-import React from "react";
+"use client";
 
-import Image from "next/image";
+import React, { useState } from "react";
 
 import { MdApps } from "react-icons/md";
-
-import imgLogo from "/public/images/logo.png";
 
 interface DashboardNavbarProps {
   sidebarOpen: boolean;
@@ -15,14 +13,28 @@ const DashboardNavbar: React.FC<DashboardNavbarProps> = ({
   sidebarOpen,
   setSidebarOpen,
 }) => {
+  const [theme, setTheme] = useState<string>("app");
+
   return (
     <div className="navbar bg-base-300 rounded-2xl px-4">
       <div className="navbar-start">
         <label className="swap swap-rotate">
-          {/* this hidden checkbox controls the state */}
-          <input type="checkbox" className="theme-controller" value="dark" />
+          <input
+            type="checkbox"
+            className="theme-controller"
+            value={theme}
+            onChange={() => {
+              const defaultTheme =
+                localStorage.getItem("theme") === "dark" ? "app" : "dark";
 
-          {/* sun icon */}
+              localStorage.setItem("theme", defaultTheme);
+              setTheme(defaultTheme);
+              document
+                .querySelector("html")
+                ?.setAttribute("data-theme", defaultTheme);
+            }}
+          />
+
           <svg
             className="swap-on fill-current w-6 h-6 ml-4"
             xmlns="http://www.w3.org/2000/svg"
@@ -31,7 +43,6 @@ const DashboardNavbar: React.FC<DashboardNavbarProps> = ({
             <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
 
-          {/* moon icon */}
           <svg
             className="swap-off fill-current w-6 h-6 ml-4"
             xmlns="http://www.w3.org/2000/svg"
